2e897c4e8f99330e8633d635d252c22f3d7cedeb
Johannes Christopher Janka <johannes.janka@hotmail.com>
2024-08-10 20:36:09 +0200

    Added movie and review impl

--- src/main/java/at/saekenz/cinerator/model/Movie.java
+++ src/main/java/at/saekenz/cinerator/model/Movie.java
@@ -1,11 +1,90 @@
 package at.saekenz.cinerator.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.List;
 
 @Entity
+@Table(name = "movies")
 public class Movie {
 
-    private @Id @GeneratedValue long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long movie_id;
+
+    private String title;
+    private Date release_date;
+    private String director;
+    private String genre;
+    private String country;
+
+    @OneToMany(mappedBy = "movie")
+    private List<Review> reviews;
+
+    public Movie() {
+
+    }
+
+    public Movie(String title, String director, Date release_date, String genre,
+                 String country, List<Review> reviews) {
+        this.title = title;
+        this.director = director;
+        this.release_date = release_date;
+        this.genre = genre;
+        this.country = country;
+        this.reviews = reviews;
+    }
+
+    public Long getMovie_id() {
+        return movie_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(Date release_date) {
+        this.release_date = release_date;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
 }
--- src/main/java/at/saekenz/cinerator/model/Review.java
+++ src/main/java/at/saekenz/cinerator/model/Review.java
@@ -1,11 +1,81 @@
 package at.saekenz.cinerator.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import at.saekenz.cinerator.model.user.User;
+import jakarta.persistence.*;
+
+import java.util.Date;
 
 @Entity
 public class Review {
 
-    private @Id @GeneratedValue long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long review_id;
+
+    private String comment;
+    private int rating;
+    private Date review_date;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    public Review() {
+    }
+
+    public Review(String comment, int rating, Date review_date, User user, Movie movie) {
+        this.comment = comment;
+        this.rating = rating;
+        this.review_date = review_date;
+        this.user = user;
+        this.movie = movie;
+    }
+
+    public Long getReview_id() {
+        return review_id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public Date getReview_date() {
+        return review_date;
+    }
+
+    public void setReview_date(Date review_date) {
+        this.review_date = review_date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
 }
